590542d59d19ea4605b026b8f488a8a8b523e832
Introl <introl-ljl@qq.com>
2026-01-06 09:21:09 +0000

    chore：添加vercel依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://blog.introl.top',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [tailwind(), sitemap()],
 });
